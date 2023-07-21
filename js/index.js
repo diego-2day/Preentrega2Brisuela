ea0183917537da3d3ea5f5d0ae89cb8b2f5d7c7f
Diego Brisuela <dibrisuela@gmail.com>
2023-07-21 01:55:59 -0300

    Comienzo clave de la estructura

---
+++ js/index.js
@@ -0,0 +1,16 @@
+//Procesos generales
+
+alert(canciones)
+class Cancion {
+    // Constructor: es llamado al crear una nueva instancia de la clase
+    constructor(nombre, año,cantante,banda) {
+      this.nombre = nombre;
+      this.año = año;
+      this.cantante = cantante;
+      this.banda = banda;
+    }
+    agregarAcancionero(cancionero) {
+        cancionero.push(cancionNueva);
+      }}
+let cancion_uno = new Cancion("Nerd",1987,"Bov dilan","The dulands");	
+canciones.agregarAcancionero(3)
